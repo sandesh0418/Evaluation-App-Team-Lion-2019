a2ac6ff3db357a3c926160eb42f54c596d9ff2c9
Ryan Blomquist <snakey699@yahoo.com>
2019-03-13 19:14:45 -0500

    Removed dummy data from Lists.

--- Evaluation-App-Team-Lion-2019/clients/src/components/dashboard/rubricList.js
+++ Evaluation-App-Team-Lion-2019/clients/src/components/dashboard/rubricList.js
@@ -2,10 +2,6 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import axios from "axios";
 
-//dummy data
-var rubrics = ["Rubric 1", "Rubric 2", "Rubric 3"];
-
-
 function ListDisplay(props)
 {
     return props.rubrics.map(function(rubric){
