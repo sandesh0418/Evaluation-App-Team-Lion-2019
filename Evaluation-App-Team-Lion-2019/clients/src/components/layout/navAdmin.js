6aede5e8d358e473650335bf4e59e9a9a6943bbe
Kushal Pokhrel <kushalpokhrel96@gmail.com>
2019-04-18 18:26:59 -0500

    some css

--- Evaluation-App-Team-Lion-2019/clients/src/components/layout/navAdmin.js
+++ Evaluation-App-Team-Lion-2019/clients/src/components/layout/navAdmin.js
@@ -1,74 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import PropTypes from 'prop-types';
-import {connect } from 'react-redux';
-import { logoutUser} from '../../actions/authActions';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import "../../stylesheets/navAdmin.css";
 
-import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 function User() {
-    return(
-        "Hi "+localStorage.name+"!"
-    );
+  return "Hi " + localStorage.name + "!";
 }
 
-class NavAdmin extends Component 
-{
-    onLogOut(e){
-        e.preventDefault();
-        this.props.logoutUser();
-    }
-    
-    render()
-    {
-        return (
-            < >
-            <Navbar.Toggle aria-controls="basic-navbar-nav"  />
-            <Navbar.Collapse id="responsive-navbar-nav" >
-                <Nav className="ml-auto" >
-                    <Nav.Link href="/dashboard">Home</Nav.Link>
-                    <Nav.Link href="/viewSummary">View Summary</Nav.Link>
-                    <NavDropdown title="Assignments">
-                        <NavDropdown.Item href="/myAssignments">My Assignments</NavDropdown.Item>
-                        <NavDropdown.Item href="/createAssignment">Create Assignment</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Evaluators">
-                        <NavDropdown.Item href="/addEvaluator">Add Evaluator</NavDropdown.Item>
-                        <NavDropdown.Item href="/viewEvaluator">View Evaluator</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Rubrics">
-                        <NavDropdown.Item href="/rubricList">Rubric List</NavDropdown.Item>
-                        <NavDropdown.Item href="/Rubric">Create Rubric</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Reports">
-                        <NavDropdown.Item href="/summaryReport">Summary</NavDropdown.Item>
-                        <NavDropdown.Item href="/measureReport">By Measure</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="/dashboard">Past Assesments</Nav.Link>
-                    <NavDropdown title="Cycle">
-                        <NavDropdown.Item href="/cycle">Start a new Cycle</NavDropdown.Item>
-                        <NavDropdown.Item href="/cycles">View Cycles</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title={User()} class="dropdown-menu dropdown-menu-right" >
-                    <dropdownMenu right>
-                        <Nav.Link href="/editProfile">Edit Profile</Nav.Link>
-                        <Nav.Link href="/" onClick ={this.onLogOut.bind(this)}>Logout</Nav.Link>
-                        </dropdownMenu>
-                </NavDropdown>
-                </Nav> 
-            </Navbar.Collapse>
-            </> 
-        );
-    }
+class NavAdmin extends Component {
+  onLogOut(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/dashboard">Home</Nav.Link>
+            <Nav.Link href="/viewSummary">View Summary</Nav.Link>
+            <NavDropdown title="Assignments">
+              <NavDropdown.Item href="/myAssignments">
+                My Assignments
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/createAssignment">
+                Create Assignment
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Evaluators">
+              <NavDropdown.Item href="/addEvaluator">
+                Add Evaluator
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/viewEvaluator">
+                View Evaluator
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Rubrics">
+              <NavDropdown.Item href="/rubricList">
+                Rubric List
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/Rubric">Create Rubric</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Reports">
+              <NavDropdown.Item href="/summaryReport">Summary</NavDropdown.Item>
+              <NavDropdown.Item href="/measureReport">
+                By Measure
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="/dashboard">Past Assesments</Nav.Link>
+            <NavDropdown title="Cycle">
+              <NavDropdown.Item href="/cycle">
+                Start a new Cycle
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/cycles">View Cycles</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown
+              title={User()}
+              class="dropdown-menu dropdown-menu-right"
+            >
+              <dropdownMenu right>
+                <Nav.Link href="/editProfile">Edit Profile</Nav.Link>
+                <Nav.Link href="/" onClick={this.onLogOut.bind(this)}>
+                  Logout
+                </Nav.Link>
+              </dropdownMenu>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </>
+    );
+  }
 }
 
 NavAdmin.propTypes = {
-    auth: PropTypes.object.isRequired,
-    logoutUser: PropTypes.func.isRequired
-}
+  auth: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired
+};
 
 const mapStateToProps = state => ({
-    auth: state.auth
-})
+  auth: state.auth
+});
 
-export default connect(mapStateToProps, {logoutUser})(NavAdmin);
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(NavAdmin);
--- Evaluation-App-Team-Lion-2019/routes/api/Coordinator.js
+++ Evaluation-App-Team-Lion-2019/routes/api/Coordinator.js
@@ -1,103 +1,137 @@
 const express = require("express");
 const router = express.Router();
-var connection = require('../../models/User');
-const passport = require('passport');
+var connection = require("../../models/User");
+const passport = require("passport");
 
-var uniqid = require('uniqid');
+var uniqid = require("uniqid");
 
-router.post('/addCoordinator', passport.authenticate("jwt", {session: false}), (req, res) =>{
-
+router.post(
+  "/addCoordinator",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     var email = req.body.email;
-    var department= req.body.department;
-    
-    var cwid = (Number) (req.body.cwid);
+    var department = req.body.department;
+
+    var cwid = Number(req.body.cwid);
     var dept_id = uniqid();
-    connection.query("SELECT * from `users` where email = ? OR CWID = ?",[email, cwid], function(err, result, fields){
+    connection.query(
+      "SELECT * from `users` where email = ? OR CWID = ?",
+      [email, cwid],
+      function(err, result, fields) {
         if (err) throw err;
 
-        if(result.length>0){
-            return res.status(400).json({ email: "Coordinator already exists" });
+        if (result.length > 0) {
+          return res.status(400).json({ email: "Coordinator already exists" });
         }
-        
-        connection.query("INSERT INTO users(`CWID`, `email`, `Dept_Id`, `role`) VALUES(?,?,?,?)",[cwid,email, dept_id , "Administrator"], function(err, result, fields){
-            if(err) throw err;
 
-            connection.query("INSERT INTO  `department`(department_Id , department_Name) Values(?,?)",[dept_id, department], function(err, result, fields){
+        connection.query(
+          "INSERT INTO users(`CWID`, `email`, `Dept_Id`, `role`) VALUES(?,?,?,?)",
+          [cwid, email, dept_id, "Administrator"],
+          function(err, result, fields) {
+            if (err) throw err;
+
+            connection.query(
+              "INSERT INTO  `department`(department_Id , department_Name) Values(?,?)",
+              [dept_id, department],
+              function(err, result, fields) {
                 if (err) throw err;
 
-              
-                    res.send('Coordinator has been added');
-                
-            })
-        })
-    })
-})
+                res.send("Coordinator has been added");
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+);
 
-
-router.get('/viewCoordinator', passport.authenticate("jwt", {session: false}), (req, res) =>{
+router.get(
+  "/viewCoordinator",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     var Coordinator = [];
-    connection.query("SELECT * from users where role =? and password!='deleted' ","Administrator", function(err, result, fields){
-        // 
+    connection.query(
+      "SELECT * from users where role =? and password!='deleted' ",
+      "Administrator",
+      function(err, result, fields) {
+        //
         console.log(result);
         if (err) throw err;
 
-        if(result.length>0){
-            for(var i = 0; i<result.length; i++){
-                Coordinator[i] = {
-                    firstName: `${result[i].firstName}`,
-                    lastName: `${result[i].lastName}`,
-                    email: `${result[i].email}`
-                }
-            }
+        if (result.length > 0) {
+          for (var i = 0; i < result.length; i++) {
+            Coordinator[i] = {
+              firstName: `${result[i].firstName}`,
+              lastName: `${result[i].lastName}`,
+              email: `${result[i].email}`
+            };
+          }
         }
 
         res.send(Coordinator);
-    })
-})
+      }
+    );
+  }
+);
 
-router.get('/viewCoordinatorDeleted', passport.authenticate("jwt", {session: false}), (req, res) =>{
+router.get(
+  "/viewCoordinatorDeleted",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     var Coordinator = [];
-    connection.query("SELECT * from users where role =? and password='deleted'","Administrator", function(err, result, fields){
-        // 
-        // 
+    connection.query(
+      "SELECT * from users where role =? and password='deleted'",
+      "Administrator",
+      function(err, result, fields) {
+        //
+        //
         if (err) throw err;
-        console.log(result)
-        if(result.length>0){
-            for(var i = 0; i<result.length; i++){
-                Coordinator[i] = {
-                    firstName: `${result[i].firstName}`,
-                    lastName: `${result[i].lastName}`,
-                    email: `${result[i].email}`
-                }
-            }
+        console.log(result);
+        if (result.length > 0) {
+          for (var i = 0; i < result.length; i++) {
+            Coordinator[i] = {
+              firstName: `${result[i].firstName}`,
+              lastName: `${result[i].lastName}`,
+              email: `${result[i].email}`
+            };
+          }
         }
 
         res.send(Coordinator);
-    })
-})
+      }
+    );
+  }
+);
 
-router.post('/removeCoordinator', passport.authenticate("jwt", {session: false}), (req, res) =>{
-
+router.post(
+  "/removeCoordinator",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     var email = req.body.email;
     console.log(email);
-    connection.query("SELECT * from `users` where email = ?",[email], function(err, result, fields){
-        if (err) throw err;
+    connection.query("SELECT * from users where email = ?", [email], function(
+      err,
+      result,
+      fields
+    ) {
+      if (err) throw err;
 
-        if(result.length<=0){
-            // return res.status(400).json({ "Coordinator doesn't exist" });
+      if (result.length <= 0) {
+        // return res.status(400).json({ "Coordinator doesn't exist" });
+      }
+
+      connection.query(
+        "UPDATE users SET password='deleted' where email =?",
+        [email],
+        function(err, result, fields) {
+          if (err) throw err;
+
+          res.send("Coordinator has been removed");
         }
-        
-        connection.query("UPDATE users SET password='deleted' where email =?",[email], function(err, result, fields){
-            if(err) throw err;
-
-
-              
-                    res.send('Coordinator has been removed');
-                
-            
-        })
-    })
-})
-
+      );
+    });
+  }
+);
 
 module.exports = router;
