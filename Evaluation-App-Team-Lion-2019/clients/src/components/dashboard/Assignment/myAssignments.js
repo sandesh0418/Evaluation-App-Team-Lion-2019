1cee7336f4d476a233775be45e577c0e9ae2909a
sandesh <bluebirdsunshine6@gmail.com>
2019-04-25 19:34:00 -0500

    incomplete assignments and complete assignments are in different column

--- Evaluation-App-Team-Lion-2019/clients/src/components/dashboard/Assignment/myAssignments.js
+++ Evaluation-App-Team-Lion-2019/clients/src/components/dashboard/Assignment/myAssignments.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import axios from "axios";
 import AddSubjects from './addSubjects';
 import './myAssignments.css';
-
+import Loader from 'react-loader-spinner';
+import { isNullOrUndefined } from 'util';
 
 function ListDisplay(props)
 {
     let list = props.assignments.map(a => {
-        if (a.finished === props.finished)
+        if (a.finished === true)
         {
             return (
                 <div className="m-3 p-3 border rounded" key={a.assignmentId}>
@@ -55,12 +56,12 @@ function ListDisplay(props)
         }
     });
 
-    if(list[0] !== null)
+    if(props.assignments.length >0)
     {
-        let type = props.finished ? "Complete" : "Incomplete"
+        
         return (
-            <div>
-                <h2>{type + " Assignments"}</h2>
+            <div id="complete">
+                <h2>{"Complete" + " Assignments"}</h2>
                 {list}
             </div>
         )
@@ -70,6 +71,75 @@ function ListDisplay(props)
         return null;
     }
 }
+
+
+function ListIncomplete(props)
+{
+    let list = props.assignments.map(a => {
+        if (a.finished === false)
+        {
+            return (
+                <div className="m-3 p-3 border rounded" key={a.assignmentId}>
+                    <p className="h5">{"Outcome name: " + a.outcomeName}</p>
+                    <div>{"Outcome description: " + a.outcomeDescription}</div>
+                    <p className="h5">{"Measure name: " + a.measureName}</p>
+                    {a.subjects[0] && a.subjects[0].subjectId !== null ? 
+                        <>
+                            <button 
+                                type="button" 
+                                className="btn btn-primary mt-2 mb-2" 
+                                id={a.rubricId + "/" + a.assignmentId}
+                                onClick={props.onClick}>
+                                {"Evaluate " + a.toolName}
+                            </button>
+                            <details>
+                                <summary>Subjects:</summary>
+                                <table className="table table-bordered p-3">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Id</th> 
+                                            <th>Graded?</th>
+                                            <th>Remove Subject</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <SubjectList 
+                                            assignmentId={a.assignmentId} 
+                                            subjects={a.subjects}
+                                            removeSubject={props.removeSubject} 
+                                        />
+                                    </tbody>
+                                </table>
+                            </details>
+                        </>
+                    : <p>There are no subjects in this assignments yet. Add subjects to evaluate.</p> }
+                    <AddSubjects assignmentId={a.assignmentId} />
+                </div>
+            )
+        }
+        else
+        {
+            return null;
+        }
+    });
+
+    if(props.assignments.length >0)
+    {
+      
+        return (
+            <div id="incomplete">
+                <h2>{"Incomplete" + " Assignments"}</h2>
+                {list}
+            </div>
+        )
+    }
+    else
+    {
+        return null;
+    }
+}
+
 
 function SubjectList(props)
 {
@@ -161,32 +231,45 @@ export default class RubricList extends Component
     {
         if (this.state.assignments === null)
         {
-            return <p>loading...</p>
+            return <Loader 
+            type="Oval"
+            
+            color="black"
+            height="100"	
+            width="100"
+         />
         }
         else
         {
             return(
                 <div>
                     <h1>My Assignments</h1>
-                    {this.state.assingments !== [] ? 
+                    <div className="row">
+                   
+                    {this.state.assignments !== [] ? 
                         <>
                             {/* show incomplete assignments */}
-                            <ListDisplay
+                            <div className="col-sm-6">
+                            <ListIncomplete
                                 finished={false} 
-                                assignments={this.state.assignments} 
+                                assignments={this.state.assignments[1]} 
                                 onClick={this.handleEvaluateClick}
                                 removeSubject={this.removeSubject} 
                             />
+                            </div>
                             {/* show complete assignments */}
+                            <div className="col-sm-6">
                             <ListDisplay
                                 finished={true} 
-                                assignments={this.state.assignments} 
+                                assignments={this.state.assignments[0]} 
                                 onClick={this.handleEvaluateClick}
                                 removeSubject={this.removeSubject} 
-                            /> 
+                            /> </div>
                         </>
                         : <p>You have no assignments.</p>
                     }
+                    
+                     </div>
                 </div>
             );
         }
