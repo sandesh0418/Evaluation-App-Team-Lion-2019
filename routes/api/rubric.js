2379497085128562b346ed1509ea72cdab846f9e
kushalpokhrel <46981112+kushalpokhrel@users.noreply.github.com>
2019-04-27 19:23:40 -0500

    Update rubric.js

--- routes/api/rubric.js
+++ routes/api/rubric.js
@@ -28,7 +28,7 @@ router.post("/createRubric",  (req,res) =>{
         let rubric_Id = uniqid();
         let Cycle_Id = req.body.Cycle_Id;
         cycle ={
-            Rubric_Id: ruZzsxdezxxcxzaassaabric_Id
+            Rubric_Id: rubric_Id
         }
         if(req.body.weight){
             weight = 1;
